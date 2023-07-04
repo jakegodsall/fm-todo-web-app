41724a68bc8db5ca06f7966ba238617bb307f3fd
jakegodsall <jake.edward.godsall@gmail.com>
2023-07-04 07:48:10 +0200

    added colours, font size etc to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    darkMode: 'class',
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            gradientColorStops: {
+                stop1: 'var(--linear-gradient1)',
+                stop2: 'var(--linear-gradient2)',
+            },
+            backgroundColor: {
+                primary: 'var(--color-bg-primary)',
+                secondary: 'var(--color-bg-secondary)',
+            },
+            textColor: {
+                primary: 'var(--color-text-primary)',
+                secondary: 'var(--color-text-secondary)',
+            },
+            fontSize: {
+                primary: '18px',
+            },
+            backgroundImage: {
+                'desktop-dark': 'url("/assets/images/bg-desktop-dark.jpg")',
+                'desktop-light': 'url("/assets/images/bg-desktop-light.jpg")',
+                'mobile-dark': 'url("/assets/images/bg-mobile-dark.jpg")',
+                'mobile-light': 'url("/assets/images/bg-mobile-light.jpg")',
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
