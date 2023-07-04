613ff7e11fde181adea9cd50303e45b89d93e289
jakegodsall <jake.edward.godsall@gmail.com>
2023-07-04 08:32:13 +0200

    added Card UI wrapper component

---
+++ src/app/components/UI/Card.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Card = ({ children }) => {
+    return <div className='bg-primary rounded-md mx-2 my-8 flex  justify-center'>{children}</div>;
+};
+
+export default Card;
